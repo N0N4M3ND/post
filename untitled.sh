84aba0aa70b1f550300dab9634c1ef96ca1d9fb2
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-07-31 17:52:12 +0400

    Create untitled.sh

---
+++ untitled.sh
@@ -0,0 +1,18 @@
+git clone https://github.com/N0N4M3ND/dotfiles-dark &&
+mv dotfiles-dark/.bashrc ~/ &&
+mv dotfiles-dark/.config ~/ &&
+chmod +x .config/bspwm/bspwmrc &&
+git clone https://github.com/N0N4M3ND/wallpapers-dark &&
+sudo mv wallpapers-dark /usr/share/backgrounds &&
+git clone https://aur.archlinux.org/cava &&
+cd cava &&
+makepkg -si &&
+cd .. &&
+git clone https://aur.archlinux.org/lightdm-webkit2-theme-glorious &&
+cd lightdm-webkit2-theme-glorious &&
+makepkg -si &&
+sudo sed -i 's/debug_mode          = false/debug_mode          = true/g' /etc/lightdm/lightdm-webkit2-greeter.conf &&
+sudo sed -i 's/detect_theme_errors = true/detect_theme_errors = false/g' /etc/lightdm/lightdm-webkit2-greeter.conf &&
+sudo sed -i 's/webkit_theme        = antergos/webkit_theme        = glorious/g' /etc/lightdm/lightdm-webkit2-greeter.conf &&
+cd .. &&
+rm -rf *
