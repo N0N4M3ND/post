3013455b645349038c1c51ea72e000a971029ecc
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-09-28 00:35:27 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -1,8 +1,7 @@
 git clone https://github.com/N0N4M3ND/dotfiles &&
 mv dotfiles/.config ~/ &&
 chmod +x .config/bspwm/bspwmrc &&
-git clone https://github.com/N0N4M3ND/wallpapers &&
-sudo mv wallpapers /usr/share/backgrounds &&
+sudo mv ~/dotfiles/wallpapers /usr/share/backgrounds &&
 git clone https://aur.archlinux.org/ttc-iosevka &&
 cd ttc-iosevka &&
 makepkg -si &&
