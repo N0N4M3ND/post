5d7f4a797ee9579f4e2ae3b2f7d27291c2c15771
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-10-17 00:42:45 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -1,6 +1,7 @@
 git clone https://aur.archlinux.org/ly &&
 cd ly &&
 makepkg -si &&
+sudo systemctl enable ly &&
 cd .. &&
 git clone https://github.com/N0N4M3ND/dotfiles &&
 mv dotfiles/.config ~/ &&
