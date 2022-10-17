578bea863f62992e2254716bf66e3aa4bafcb7ff
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-10-17 19:38:17 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -3,10 +3,6 @@ cd ly &&
 makepkg -si &&
 sudo systemctl enable ly &&
 cd .. &&
-git clone https://aur.archlinux.org/ttf-font-awesome &&
-cd ttf-font-awesome &&
-makepkg -si &&
-cd .. &&
 git clone https://github.com/N0N4M3ND/dotfiles &&
 mv dotfiles/.config ~/ &&
 chmod +x .config/bspwm/bspwmrc &&
