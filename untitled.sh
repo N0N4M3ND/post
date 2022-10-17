65be173d22a6a774dfb980a54083039d759067ec
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-10-17 12:45:11 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -6,8 +6,4 @@ cd .. &&
 git clone https://github.com/N0N4M3ND/dotfiles &&
 mv dotfiles/.config ~/ &&
 chmod +x .config/bspwm/bspwmrc &&
-git clone https://aur.archlinux.org/ttc-iosevka &&
-cd ttc-iosevka &&
-makepkg -si &&
-cd .. &&
 rm -rf *
