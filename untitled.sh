4eccb1a2f9c4128dc5ec55fd6a0f4362cc193e03
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-10-19 11:07:01 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -3,6 +3,10 @@ cd ly &&
 makepkg -si &&
 sudo systemctl enable ly &&
 cd .. &&
+git clone https://aur.archlinux.org/ttf-iosevka &&
+cd ttf-iosevka &&
+makepkg -si &&
+cd .. &&
 git clone https://github.com/N0N4M3ND/dotfiles &&
 mv dotfiles/.config ~/ &&
 mv dotfiles/.wallpaper ~/ &&
