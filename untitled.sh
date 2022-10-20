5c171bc03210ae916e7be189978b4b99249336e2
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-10-20 15:29:15 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -1,8 +1,3 @@
-git clone https://aur.archlinux.org/ly &&
-cd ly &&
-makepkg -si &&
-sudo systemctl enable ly &&
-cd .. &&
 git clone https://aur.archlinux.org/ttf-iosevka &&
 cd ttf-iosevka &&
 makepkg -si &&
@@ -11,4 +6,5 @@ git clone https://github.com/N0N4M3ND/dotfiles &&
 mv dotfiles/.config ~/ &&
 mv dotfiles/.wallpaper ~/ &&
 chmod +x .config/bspwm/bspwmrc &&
+mv dotfiles/lightdm-gtk-greeter.conf/ /etc/lightdm/lightdm-gtk-greeter.cont
 rm -rf *
