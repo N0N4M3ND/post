c5bdb524845087e1dd8203f26fb9b03b79121eae
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-10-25 19:31:47 +0400

    Update untitled.sh

--- untitled.sh
+++ untitled.sh
@@ -5,7 +5,4 @@ cd .. &&
 git clone https://github.com/N0N4M3ND/dotfiles &&
 mv dotfiles/.config ~ &&
 chmod +x .config/bspwm/bspwmrc &&
-mkdir /usr/share/backgrounds &&
-mv dotfiles/lightdm-gtk-greeter.conf /etc/lightdm/lightdm-gtk-greeter.conf &&
-mv dotfiles/wallpapers /usr/share/backgrounds &&
 rm -rf *
