a4996bb2a036db98e5ea497d6a6150e4c3590002
The-Green-Man <95738087+N0N4M3ND@users.noreply.github.com>
2022-10-30 13:33:11 +0400

    Update and rename install.sh to script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,7 @@ git clone https://aur.archlinux.org/ttf-iosevka &&
 cd ttf-iosevka &&
 makepkg -si &&
 cd .. &&
-git clone https://github.com/N0N4M3ND/dotfiles &&
-mv dotfiles/.bash_profile ~ &&
-mv dotfiles/.config ~ &&
+mv post/.bash_profile ~ &&
+mv post/.config ~ &&
 chmod +x .config/bspwm/bspwmrc &&
 rm -rf *
