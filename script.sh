97eb2bb8220c6eeb0a5bbdf6202bba535232516b
n0n4m3nd <doreulinika69@gmail.com>
2022-12-10 18:30:17 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,8 @@
 cd ~ &&
 rm -rf .* &
 git clone https://aur.archlinux.org/pfetch &&
-git clone https://aur.archlinux.org/ttf-iosevka &&
 git clone https://github.com/n0n4m3nd/.wallpapers &&
 cd pfetch &&
-makepkg -si &&
-cd ~ &&
-cd ttf-iosevka &&
 makepkg -si &&
 cd ~ &&
 mv post/.bashrc ~ &&
